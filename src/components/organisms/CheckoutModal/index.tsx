51e68093128ece548489cd2a359ae781d5f86396
CleanBread <gleb4ist@gmail.com>
2021-05-14 15:24:30 +0300

    ChooseCollection func comp to class comp

---
+++ src/components/organisms/CheckoutModal/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+// import { Modal } from '../../molecules';
+
+const ChecoutModal: React.FC = () => {
+  return (
+    <div className=""></div>
+    // <Modal
+    //   isVisible={modals.auction.getIsOpen}
+    //   className="m-auction"
+    //   handleCancel={handleClose}
+    //   width={380}
+    //   destroyOnClose
+    // >
+    //   <div className="m-auction__content">
+    //   </Modal>
+  );
+};
+
+export default ChecoutModal;
