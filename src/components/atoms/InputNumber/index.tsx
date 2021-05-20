8d0ad445546cedab9e75f9ac59db3a0c62486754
gavdniwe <dimanikus0106@gmail.com>
2021-05-20 18:42:38 +0300

    removed min functional

--- src/components/atoms/InputNumber/index.tsx
+++ src/components/atoms/InputNumber/index.tsx
@@ -1,5 +1,6 @@
 import React, { ChangeEvent } from 'react';
 import { Input } from 'antd';
+import BigNumber from 'bignumber.js/bignumber';
 
 interface InputNumberProps {
   id: undefined | string;
@@ -13,7 +14,7 @@ interface InputNumberProps {
   integer?: boolean;
   positiveOnly?: boolean;
   max?: number;
-  min?: number;
+  // min?: number;
 }
 
 const InputNumber: React.FC<InputNumberProps> = ({
@@ -22,12 +23,13 @@ const InputNumber: React.FC<InputNumberProps> = ({
   className,
   value,
   size,
+  suffix,
   id,
   onBlur,
   placeholder,
   onChange,
   max,
-  min,
+  // min,
 }) => {
   const getRegex = () => {
     if (integer) {
@@ -35,6 +37,28 @@ const InputNumber: React.FC<InputNumberProps> = ({
     }
     return positiveOnly ? /^[+]?([.]\d+|\d+[.]?\d*)$/ : /^[-+]?([.]\d+|\d+[.]?\d*)$/;
   };
+
+  /*  const checkMin = (comparingValue: string) => {
+    const arrayedComparingValue = Array.from(String(comparingValue), Number);
+    const arrayedMin = Array.from(String(min), Number);
+    let index = 0;
+    for (const elem of arrayedComparingValue) {
+      if (new BigNumber(arrayedMin[index]).isLessThan(new BigNumber(elem))) return true;
+      if (
+        !(
+          (
+            new BigNumber(arrayedMin[index]).isLessThanOrEqualTo(new BigNumber(elem)) || // every symbol should be more or equal to min value
+            (isNaN(elem) && isNaN(arrayedMin[index])) || // '.' elements
+            (elem !== undefined && arrayedMin[index] === undefined)
+          ) // if arrayedComparingValue longer than arrayedMin
+        )
+      ) {
+        return false;
+      }
+      index += 1;
+    }
+  }; */
+
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const reg = getRegex();
     const inputValue = e.target.value;
@@ -43,17 +67,16 @@ const InputNumber: React.FC<InputNumberProps> = ({
       inputValue === '' ||
       (!positiveOnly && inputValue === '-')
     ) {
+      /* if (max && min) {
+        if (checkMin(inputValue) && new BigNumber(inputValue) <= new BigNumber(max)) onChange(e);
+      } else if (max) {
+        if (new BigNumber(inputValue) <= new BigNumber(max)) onChange(e);
+      } else if (min) {
+        if (checkMin(inputValue)) onChange(e);
+      } else onChange(e); */
       if (max) {
-        if (min) {
-          if (min <= +inputValue && +inputValue <= max) onChange(e);
-        }
-        if (+inputValue <= max) onChange(e);
-      } else {
-        if (min) {
-          if (min <= +inputValue) onChange(e);
-        }
-        onChange(e);
-      }
+        if (new BigNumber(inputValue) <= new BigNumber(max)) onChange(e);
+      } else onChange(e);
     }
   };
   return (
@@ -62,6 +85,7 @@ const InputNumber: React.FC<InputNumberProps> = ({
       value={value}
       className={className}
       size={size}
+      suffix={suffix}
       type="text"
       placeholder={placeholder}
       onChange={(e) => handleChange(e)}
